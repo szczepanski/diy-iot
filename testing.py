5cf3c6a3b31367bce2bb4f9580101dd7208c0a01
Piotr <ask.piotr@yahoo.co.uk>
2020-06-11 10:33:21 +0200

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,26 @@
+import time
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
+
+
+def test1():
+    kit = MotorKit()
+    print('onestep')
+    time.sleep(1)
+    kit.stepper1.onestep()
+    print('release')
+    time.sleep(1)
+    kit.stepper1.release()
+
+
+def main():
+    test1()
+
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
+
